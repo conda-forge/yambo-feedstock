cc7aebc2aee8b7c104deef75e2f7a95911ce9670
Nicola Spallanzani <nic.spalla@gmail.com>
2025-11-07 09:40:35 +0100

    MNT: Re-rendered with conda-smithy 3.52.3 and conda-forge-pinning 2025.11.06.19.20.54
    Other tools:
    - conda-build 25.9.0
    - rattler-build 0.48.1
    - rattler-build-conda-compat 1.4.9

--- recipe/build.sh
+++ recipe/build.sh
@@ -77,9 +77,6 @@ export FCFLAGS="-O3 -g -fno-lto -fopenmp ${FCFLAGS//-march=*/} -Wl,-headerpad_ma
 export FFLAGS="-O3 -g  -fno-lto -fopenmp ${FFLAGS//-march=*/} -Wl,-headerpad_max_install_names"
 export CFLAGS="-O2 -D_C_US -D_FORTRAN_US ${CFLAGS} -Wl,-headerpad_max_install_names"
 
-ls -la $BUILD_PREFIX/bin/${HOST}-gcc
-#ls -la $BUILD_PREFIX/bin/${BUILD}-gcc
-
 ./configure ${with_build} \
     --prefix="${PREFIX}" \
     --enable-mpi --enable-open-mp ${with_precision} \
